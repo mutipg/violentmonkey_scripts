ec940a160edcf6f0f163bdb2bc0befd79f9fea5b
mutipg <mutancik@gmail.com>
2023-12-06 13:15:58 +0100

    Update icons_to_osm.py

--- scripts/icons_to_osm.py
+++ scripts/icons_to_osm.py
@@ -1,24 +1,3 @@
-// ==UserScript==
-// @name        Icons
-// @namespace   Violentmonkey Scripts
-// @match       https://www.openstreetmap.org/*
-// @match       https://overpass-api.de/achavi/*
-// @match       https://rene78.github.io/latest-changes/*
-// @grant       none
-// @version     1.1.0
-// @description 2023-12-06, 12:31:37
-// @license     GNU Affero General Public License v3.0
-// @author      Marek-M
-// @updateURL
-// @downloadURL
-// ==/UserScript==
-
-(() => {
-  'use strict'
-
-
 const iconsFolder = `https://github.com/mapbox/maki/tree/main/icons/`
 
 const iconsArrayList = [a,b]
-
-})()
