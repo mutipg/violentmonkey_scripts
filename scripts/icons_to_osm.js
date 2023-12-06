7578bf251e70ca3379662d0f9586c57d7f9de913
mutipg <mutancik@gmail.com>
2023-12-06 13:24:28 +0100

    Update icons_to_osm.js

--- scripts/icons_to_osm.js
+++ scripts/icons_to_osm.js
@@ -1,3 +1,7 @@
 const iconsFolder = `https://github.com/mapbox/maki/tree/main/icons/`
-
-const iconsArrayList = [a,b]
+// icon Array ['icon_filename', 'title']
+const iconsArrayList =
+  [
+    ['defibrillator.svg',   'emergency=defibrillator']
+    ['cross.svg',           'highway=crossing']
+  ]
